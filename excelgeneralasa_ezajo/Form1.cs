6f2c556f21c1f1c20675896193b81c16e59ddeac
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2020-10-04 20:14:06 +0200

    Adatok betöltése

--- excelgeneralasa_ezajo/Form1.cs
+++ excelgeneralasa_ezajo/Form1.cs
@@ -12,9 +12,19 @@ namespace excelgeneralasa_ezajo
 {
     public partial class Form1 : Form
     {
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
     }
 }
