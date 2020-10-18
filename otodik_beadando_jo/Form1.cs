b453890f254852d071331db7b895f7f5dac8d266
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2020-10-18 15:08:34 +0200

    webszolgaltatas hivasa

--- otodik_beadando_jo/Form1.cs
+++ otodik_beadando_jo/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using otodik_beadando_jo.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,19 @@ namespace otodik_beadando_jo
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
