f92cc15cd1c8767ad16d6ae5e31393a42da07461
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2020-10-23 18:25:37 +0200

    osztályok létrehozása

---
+++ hatodik_beadando/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hatodik_beadando.Entities
+{
+    class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+    }
+}
