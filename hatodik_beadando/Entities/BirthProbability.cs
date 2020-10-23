d8c4c1412013d80fec823b7a6dc23f716798e845
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2020-10-23 19:17:26 +0200

    adatok betöltése

--- hatodik_beadando/Entities/BirthProbability.cs
+++ hatodik_beadando/Entities/BirthProbability.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace hatodik_beadando.Entities
 {
-    class BirthProbability
+    public class BirthProbability
     {
         public int Age { get; set; }
         public int NbrOfChildren { get; set; }
--- hatodik_beadando/Entities/DeathProbability.cs
+++ hatodik_beadando/Entities/DeathProbability.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace hatodik_beadando.Entities
 {
-    class DeathProbability
+    public class DeathProbability
     {
         public Gender Gender { get; set; }
         public int Age { get; set; }
