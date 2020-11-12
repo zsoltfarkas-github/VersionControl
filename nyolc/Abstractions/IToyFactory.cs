8ee4cb41713ef6812f2e89c7e82c125c533ad4cd
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2020-11-12 18:40:44 +0100

    gyárabsztrakció kezelése

---
+++ nyolc/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using nyolc.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyolc.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
--- nyolc/Abstractions/Toy.cs
+++ nyolc/Abstractions/Toy.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using nyolc.Abstraction;
 
 namespace nyolc.Abstraction
 {
