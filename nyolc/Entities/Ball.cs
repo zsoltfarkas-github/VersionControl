6423d889fa23a3e4f1d060fce353439a20dc6376
Zsolt Márk Farkas <zsolt.farkas2@stud.uni-corvinus.hu>
2020-11-12 18:05:14 +0100

    absztrakció felhasználása a labdában

--- nyolc/Entities/Ball.cs
+++ nyolc/Entities/Ball.cs
@@ -5,32 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using nyolc.Abstraction;
 
 namespace nyolc.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = Width;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-
-        public void MoveBall()
-        {
-            Left += 1;
         }
     }
 }
